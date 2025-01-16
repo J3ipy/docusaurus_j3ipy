869023e684f23885ba429c10dbf55e9092833be4
João Pedro <pedrosant1905@gmail.com>
2025-01-16 16:31:31 -0300

    modificações de imagens

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,31 +5,28 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Tech Stacks',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/1.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Atualmente trabalho com a linguagem Python, focado em soluções de IA/ML e Robótica.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Profissão',
+    Svg: require('@site/static/img/2.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Graduando em Sistema de Informação no Instituto Federal de Sergipe e Pesquisador do Laboratório de Inovação e Criatividade (LABIC).
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Linhas de Pesquisa',
+    Svg: require('@site/static/img/3.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Meus interesses de pesquisa são Robótica, Redes Neurais Artificiais e IoT.
       </>
     ),
   },
